d25abd39ce1aad67192927129292b6916a575cb6
punytan <punytan@gmail.com>
2012-04-21 02:41:02 +0900

    initial commit

---
+++ lib/DBIx/Rainy.pm
@@ -0,0 +1,32 @@
+package DBIx::Rainy;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+DBIx::Rainy -
+
+=head1 SYNOPSIS
+
+  use DBIx::Rainy;
+
+=head1 DESCRIPTION
+
+DBIx::Rainy is
+
+=head1 AUTHOR
+
+punytan E<lt>punytan@gmail.comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
